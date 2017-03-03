9d0719a42558e0d10f660a6991a4544eb0b7e58c
mliston <matthew.r.liston@gmail.com>
2017-03-03 15:43:37 +0000

    gives correct probablilities for 1 case

--- doomsday_fuel2.py
+++ doomsday_fuel2.py
@@ -5,23 +5,31 @@ def recurse(m,state,depth,prev_state,prev_odds,prob):
 #        print (1/depth)
 #        print (float(prev_odds)/float(prev_state))
 #        print 'prev odds', prev_odds, 'prev state', prev_state
-        prob[state]+=((1/float(depth))*(float(prev_odds)/float(prev_state)))
+#        prob[state]+=(float(prev_odds)/float(depth))*(float(prev_odds)/float(prev_state))
+        prob[state]+=(float(prev_odds)/float(prev_state))
 #        print 'prob[state]', prob[state]
         return prob
     else:
+        try:
+            odds=(float(prev_odds)/float(prev_state))
+        except ZeroDivisionError: 
+            odds=1
         prev_state=0
         for x in m[state]:
             prev_state+=x
         for i in range(0,len(m[state])):
 #            print m[state][i]
             if m[state][i]>0:
-                recurse(m,i,depth+1,prev_state,m[state][i],prob)
+                recurse(m,i,depth+1,prev_state,m[state][i]*odds,prob)
     return prob
 
 def answer(m):
-    return recurse(m,0,0,0,0,[0,0,0,0,0])
+    prob=[]
+    for i in range(0,len(m)):
+        prob.append(0)
+    return recurse(m,0,0,0,0,prob)
 
 print answer([[0,2,1,0,0],[0,0,0,3,4],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]])
-                
+#print answer([[0,1,0,0,0,1],[4,0,0,3,2,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]])                
             
 
